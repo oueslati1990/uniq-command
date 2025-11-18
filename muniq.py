477f24a9bea954740c4287d18b5fad279c7669a9
moueslati <eng.oueslati.mohamed@gmail.com>
2025-11-18 09:21:55 +0100

    Implement step 2 of the challenge with reading from stdin and writing to a file

--- muniq.py
+++ muniq.py
@@ -3,13 +3,13 @@ import sys
 from typing import List
 
 def read_file(filename):
-     """Read file"""
-     with open(filename, 'rb') as f:
-          return f.read()
-     
+    """Read file"""
+    with open(filename, 'rb') as f:
+        return f.read()
+
 def get_uniq_lines(content) -> List[str]:
     """Retrieve unique lines from the content of the input"""
-    
+
     dict_lines = {}
     for line in content.decode('utf-8').split('\n'):
         if line in dict_lines.keys():
@@ -18,38 +18,62 @@ def get_uniq_lines(content) -> List[str]:
             dict_lines[line] = 1
 
     return dict_lines.keys()
-     
+
 def main():
-     """
-     Main function to parse arguments and execute the appropriate counting operation.
-     """
-     parser = argparse.ArgumentParser(
-         description="uniq command"
-     )
+    """
+    Main function to parse arguments and execute the appropriate counting operation.
+    """
+    parser = argparse.ArgumentParser(
+        description="uniq command"
+    )
 
-     parser.add_argument('filename', nargs='?', default=None, 
-                         help="file to analyze")
-     
-     args = parser.parse_args()
+    parser.add_argument('filenames', nargs='*', default=None,
+                        help="input file and output file (if exists)")
 
-     content = b''
-     try:
-         if args.filename:
-            content = read_file(args.filename)
-     except FileNotFoundError:
-         print(f"{args.filename}: no such file", file=sys.stderr)
-         exit(1)
-     except PermissionError:
-         print(f"You have no permission to access this file {args.filename}", file=sys.stderr)
-         exit(1)
-     except IOError:
-         print(f"Something went wrong when reading the file {args.filename}")
-         exit(1)
+    args = parser.parse_args()
 
-     uniq_lines = get_uniq_lines(content)
+    content = b''
+    output_file = sys.stdout
+    input_filename = None
 
-     for line in uniq_lines:
-         print(line, file=sys.stdout)
+    try:
+        count = len(args.filenames)
+
+        # Handle input
+        if count == 0:
+            content = sys.stdin.buffer.read()
+        elif args.filenames[0] == '-':
+            content = sys.stdin.buffer.read()
+            input_filename = '-'
+        else:
+            input_filename = args.filenames[0]
+            content = read_file(args.filenames[0])
+
+        # Handle output
+        if count == 2:
+            output_file = args.filenames[1]
+    except FileNotFoundError:
+        print(f"{input_filename}: no such file", file=sys.stderr)
+        exit(1)
+    except PermissionError:
+        print(f"You have no permission to access this file {input_filename}", file=sys.stderr)
+        exit(1)
+    except IOError:
+        print(f"Something went wrong when reading the file {input_filename}")
+        exit(1)
+
+    uniq_lines = get_uniq_lines(content)
+
+    # Write output
+    if isinstance(output_file, str):
+        # Output to file
+        with open(output_file, 'w') as f:
+            for line in uniq_lines:
+                print(line, file=f)
+    else:
+        # Output to stdout
+        for line in uniq_lines:
+            print(line, file=output_file)
 
 
 if __name__ == '__main__':
